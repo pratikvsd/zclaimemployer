195599404cf2d4de12ff495d53a3af6d6cc6fa17
tanyashroff00 <tanyashroff00@gmaiil.com>
2022-09-16 09:47:31 -0700

    third commit

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -9,11 +9,51 @@ sap.ui.define([
 			var oModel = this.getOwnerComponent().getModel("employerList");
 			this.getView().setModel(oModel);
 		},
+		clickClaimBtn: function() {
+			if (!this.empDialog) {
+				this.empDialog = sap.ui.xmlfragment("safetysuitezclaimemployer.fragment.claimWizard", this);
+				this.getView().addDependent(this.empDialog);
+			}
+			this.empDialog.open();
+		},
 
-		onPage2: function() {
-			var page = this.getView().getParent();
-			page.to("detailPage", "flip");
+		handleWizardCancel: function(oEvent) {
+			this.empDialog.close();
+		},
 
+		onDialogBackButton: function() {
+			this._iSelectedStepIndex = this._oWizard.getCurrentStep();
+			var oPreviousStep = this._oWizard.getSteps()[this._iSelectedStepIndex - 1];
+
+			if (this._oSelectedStep) {
+				this._oWizard.goToStep(oPreviousStep, true);
+			} else {
+				this._oWizard.previousStep();
+			}
+
+			this._iSelectedStepIndex--;
+			this._oSelectedStep = oPreviousStep;
+		},
+
+		onDialogNextButton: function() {
+			this._oWizard = sap.ui.getCore().byId("claimFormWizard");
+			this._iSelectedStepIndex = this._oWizard.getCurrentStep();
+			var oNextStep = this._oWizard.getSteps()[this._iSelectedStepIndex + 1];
+
+			if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+				this._oWizard.goToStep(oNextStep, true);
+			} else {
+				this._oWizard.nextStep();
+			}
+
+			this._iSelectedStepIndex++;
+			this._oSelectedStep = oNextStep;
+		},
+
+		handleNavigationChange: function(oEvent) {
+			this._oSelectedStep = oEvent.getParameter("step");
+			this._iSelectedStepIndex = this._oWizard.getSteps().indexOf(this._oSelectedStep);
+			this.handleButtonsVisibility();
 		}
 
 	});
